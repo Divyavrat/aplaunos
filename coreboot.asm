0120f5e75f894b49bbba14d615afdd039e8f39fb
Divyavrat Jugtawat <divyavrat@gmail.com>
2025-06-15 15:15:12 +0530

    core os fixes

--- coreboot.asm
+++ coreboot.asm
@@ -8,31 +8,34 @@ nop
 ; Note: some of these values are hard-coded in the source!
 ; Values are those used by IBM for 1.44 MB, 3.5" diskette
 
-bpbOEM			db "My OS   " ; Disk label
-bpbBytesPerSector:  	DW 512 ; Bytes per sector
-bpbSectorsPerCluster: 	DB 1 ; Sectors per cluster
-bpbReservedSectors: 	DW 1 ; Reserved sectors for boot record
-bpbNumberOfFATs: 	DB 2 ; Number of copies of the FAT
-bpbRootEntries: 	DW 224 ; Number of entries in root dir
+; BPB and root dir variables for USB/FAT16
+bpbOEM          db "My OS   " ; Disk label
+bpbBytesPerSector:      DW 512 ; Bytes per sector
+bpbSectorsPerCluster:   DB 1 ; Sectors per cluster
+bpbReservedSectors:     DW 1 ; Reserved sectors for boot record
+bpbNumberOfFATs:    DB 2 ; Number of copies of the FAT
+bpbRootEntries:     DW 224 ; Number of entries in root dir
 ; (224 * 32 = 7168 = 14 sectors to read)
-bpbTotalSectors: 	DW 2880 ; Number of logical sectors
-bpbMedia: 		DB 0xf0  ;; 0xF1 ; Medium descriptor byte
-bpbSectorsPerFAT: 	DW 9 ; Sectors per FAT
-bpbSectorsPerTrack: 	DW 18 ; Sectors per track (36/cylinder)
-bpbHeadsPerCylinder: 	DW 2 ; Number of sides/heads
-bpbHiddenSectors: 	DD 0 ; Number of hidden sectors
+bpbTotalSectors:    DW 2880 ; Number of logical sectors
+bpbMedia:       DB 0xf0  ;; 0xF1 ; Medium descriptor byte
+bpbSectorsPerFAT:   DW 9 ; Sectors per FAT
+bpbSectorsPerTrack:     DW 18 ; Sectors per track (36/cylinder)
+bpbHeadsPerCylinder:    DW 2 ; Number of sides/heads
+bpbHiddenSectors:   DD 0 ; Number of hidden sectors
 bpbTotalSectorsBig:     DD 0 ; Number of LBA sectors
-bsDriveNumber: 	        DB 0 ; Drive No: 0
-bsUnused: 		DB 0
-bsExtBootSignature: 	DB 0x29;0x41 ; Drive signature: 41 for floppy
-bsSerialNumber:	        DD 0xa0a1a2a3 ; Volume ID: any number
-bsVolumeLabel: 	        DB "MOS FLOPPY " ; Volume Label: any 11 chars
-bsFileSystem: 	        DB "FAT12   " ; File system type: don't change!
+bsDriveNumber:          DB 0 ; Drive No: 0
+bsUnused:       DB 0
+bsExtBootSignature:     DB 0x29;0x41 ; Drive signature: 41 for floppy
+bsSerialNumber:         DD 0xa0a1a2a3 ; Volume ID: any number
+bsVolumeLabel:          DB "MOS FLOPPY " ; Volume Label: any 11 chars
+bsFileSystem:           DB "FAT12   " ; File system type: don't change!
+root_dir_sector        dw 0
+root_dir_size          dw 0
 
 main:
 
 ; Save boot drive number
-; mov [boot_drive], dl
+mov [boot_drive], dl    ; Save boot drive number (BIOS puts it in DL)
 
 ; Initialize segment registers
 cli                 ; Disable interrupts
@@ -41,33 +44,49 @@ mov ds, ax
 mov es, ax
 mov fs, ax
 mov gs, ax
-mov ax, 0x0000				; set the stack
+mov ax, 0x0000      ; set the stack
 mov ss, ax
 mov sp, 0xFFFF
 sti                 ; Enable interrupts
 
-mov ah, 0x0E            ; BIOS Teletype Output function
-mov al, 'H'             ; Character to print (ASCII for 'H')
-xor bh, bh              ; Page number (usually 0 for text mode)
-int 0x10                ; Call BIOS video interrupt
+; Read BPB from boot sector (0x7C00)
+mov ax, 0x07C0
+mov ds, ax
+mov ax, [0x0B]      ; bytes per sector
+mov [bpbBytesPerSector], ax
+mov al, [0x0D]      ; sectors per cluster
+mov [bpbSectorsPerCluster], al
+mov ax, [0x0E]      ; reserved sectors
+mov [bpbReservedSectors], ax
+mov al, [0x10]      ; number of FATs
+mov [bpbNumberOfFATs], al
+mov ax, [0x11]      ; root entries
+mov [bpbRootEntries], ax
+mov ax, [0x13]      ; total sectors
+mov [bpbTotalSectors], ax
+mov al, [0x15]      ; media descriptor
+mov [bpbMedia], al
+mov ax, [0x16]      ; sectors per FAT
+mov [bpbSectorsPerFAT], ax
 
-mov al, 'e'
-int 0x10
+; Calculate root directory sector
+mov al, [bpbNumberOfFATs]
+mov ah, 0
+mov cx, [bpbSectorsPerFAT]
+mul cx                  ; ax = num_fats * sectors_per_fat
+add ax, [bpbReservedSectors] ; ax = reserved_sectors + (num_fats * sectors_per_fat)
+mov [root_dir_sector], ax
 
-mov al, 'l'
-int 0x10
-
-mov al, 'l'
-int 0x10
-
-mov al, 'o'
-int 0x10
-
-mov al, '!'
-int 0x10
+; Calculate root directory size (in sectors)
+mov ax, [bpbRootEntries]
+mov bx, 32
+mul bx                  ; ax = root_entries * 32
+div word [bpbBytesPerSector] ; ax = size in sectors
+mov [root_dir_size], ax
 
 
 ; text mode
+mov ah, 0x0E            ; BIOS Teletype Output function
 pusha
 mov ax, 3
 mov bx, 0
@@ -78,16 +97,45 @@ popa
 mov si, kernel_filename
 call print_string
 
-; Load FAT12 root directory
-mov ax, 19          ; Root directory starts at sector 19
+; Load FAT16 root directory
+mov si, [root_dir_sector]
+mov cx, [root_dir_size]
 mov bx, 0x1000      ; Load root directory at 0x1000
-call read_sector
+
+read_root_dir_loop:
+    mov ax, si
+    call read_sector
+    add bx, [bpbBytesPerSector]
+    inc si
+    loop read_root_dir_loop
 
 ; Search for kernel.com in root directory
 mov di, 0x1000      ; Start of root directory
-mov cx, 224         ; Number of root directory entries
+mov cx, [bpbRootEntries]         ; Number of root directory entries
 search_kernel:
     push cx
+    ; Print current filename being checked
+    push di
+    push si
+    mov si, di
+    mov cx, 11      ; Filename length
+print_filename:
+    mov al, [si]
+    mov ah, 0x0E    ; BIOS teletype function
+    int 0x10
+    inc si
+    loop print_filename
+    ; Print newline
+    mov al, 13      ; Carriage return
+    int 0x10
+    mov al, 10      ; Line feed
+    int 0x10
+    ; Wait for key press
+    mov ah, 0x00    ; BIOS get key function
+    int 0x16        ; Wait for key press
+    pop si
+    pop di
+    
     mov cx, 11      ; Filename length
     mov si, kernel_filename
     push di
